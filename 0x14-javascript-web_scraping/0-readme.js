72610ab0959a077361cb8d14831b891fee63b6ee
Mr_Darling <117775164+MrDarlingTheEngineer@users.noreply.github.com>
2024-08-01 04:33:45 +0000

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -10,9 +10,11 @@
 const fs = require('fs');
 
 const filePath = process.argv[2];
-try {
-  const data = fs.readFileSync(filePath, 'utf8');
-  console.log(data);
-} catch (err) {
-  console.error(err);
-}
+
+fs.readFile(filePath, 'utf-8', (error, data) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(data);
+  }
+});
