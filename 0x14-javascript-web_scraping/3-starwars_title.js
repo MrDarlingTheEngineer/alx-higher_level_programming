453e7c481b2eaedaae9c0c16101bd55db39ab959
Mr_Darling <117775164+MrDarlingTheEngineer@users.noreply.github.com>
2024-08-01 04:39:43 +0000

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,7 @@
 
 const req = require('request');
 
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 req(url, function (error, response, body) {
   if (error) console.log(error);
   else if (response.statusCode === 200) {
